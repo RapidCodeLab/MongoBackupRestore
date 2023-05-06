3447ad03dd03e672c956a2797895f22b3292df45
Pavel Sanicovich <rapidcodelab.mx@gmail.com>
2023-05-06 18:45:17 +0300

    added something

--- backup-task.sh
+++ backup-task.sh
@@ -1,6 +1,14 @@
-!#/bin/bash
+#!/bin/bash
 
-# check direcory exists, make if not
+_backupdir="mongobackups"
+# check direcory exists, make if not exists
+if [ ! -d $_backupdir ]
+then
+  mkdir $_backupdir      
+fi  
+#
+d=`date '+%d-%m-%y'`  
+echo $d
 #
 # check ammount of backup files, remove first if more than ammount from settings
 #
