c4378de3265b84e01780a572b40cc76a1bf297ae
Pavel Sanicovich <rapidcodelab.mx@gmail.com>
2023-05-07 01:21:17 +0300

    added some code

--- backup-task.sh
+++ backup-task.sh
@@ -18,7 +18,8 @@ d=`date '+%d-%m-%y'`
 # 
 # Make last mongo backup with current date in filename
 docker exec -i $_container /usr/bin/mongodump --db $_dbname --out $_dump 
-docker cp $_container:$_dump $_backupdir
+_container_path="$_container:$_dump"
+docker cp $_container_path $_backupdir
 tar czf $_backupdir+"/"+$d+".tar.gz"
 # Exit with done message
 exit 0
