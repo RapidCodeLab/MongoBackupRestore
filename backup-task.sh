1b5278bc9c1104f1ab24342c27c610c1c8f912e7
Pavel Sanicovich <rapidcodelab.mx@gmail.com>
2023-05-07 01:10:33 +0300

    added some backup code

--- backup-task.sh
+++ backup-task.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 _backupdir="mongobackups"
+_container="mongoddb"
+_dbname="gopusher"
+_dump="/dump"
+
 # check direcory exists, make if not exists
 if [ ! -d $_backupdir ]
 then
   mkdir $_backupdir      
 fi  
-#
+
+# making date for filename
 d=`date '+%d-%m-%y'`  
-echo $d
 #
-# check ammount of backup files, remove first if more than ammount from settings
-#
-# write last backup 
-#
+# Count of backup files, delete first if more than amount from settings 
+# 
+# Make last mongo backup with current date in filename
+docker exec -i $_container /usr/bin/mongodump --db $_dbname --out $_dump 
+docker cp $_container:$_dump $_backupdir
+tar czf $_backupdir+"/"+$d+".tar.gz"
+# Exit with done message
+exit 0
